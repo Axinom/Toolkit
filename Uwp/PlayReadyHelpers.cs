2ef83ee8d95fa20998d9e9fea043d532250c0cdc
Sander Saares <saares@axinom.com>
2015-10-06 15:55:23 +0300

    6.1.0  * Added support for automatic domain joining when acquiring PlayReady licenses.  * Added Helpers.PlayReady.FulfillServiceRequest().

--- Uwp/PlayReadyHelpers.cs
+++ Uwp/PlayReadyHelpers.cs
@@ -8,6 +8,8 @@
 
 	public static partial class UwpHelpers
 	{
+		private static readonly LogSource PlayReadyLog = Log.Default.CreateChildSource("Helpers.PlayReady");
+
 		/// <summary>
 		/// Ensures that PlayReady is activated for the current application. Any other PlayReady operations may fail
 		/// before activation, so you are recommended to activate at the earliest possible opportunity.
@@ -19,7 +21,7 @@
 				// This will throw if PlayReady is not activated.
 				var temp = PlayReadyStatics.PlayReadySecurityVersion;
 
-				Log.Default.Info("PlayReady is already activated.");
+				PlayReadyLog.Info("PlayReady is already activated.");
 				return;
 			}
 			catch (Exception ex)
@@ -28,35 +30,37 @@
 				if (ex.HResult != unchecked((int)0x8004B822))
 					throw;
 
-				Log.Default.Info("PlayReady requires activation.");
+				PlayReadyLog.Info("PlayReady requires activation. Activating...");
 
 				var request = new PlayReadyIndividualizationServiceRequest();
 				await request.BeginServiceRequest().IgnoreContext();
 
-				Log.Default.Info("PlayReady activated.");
+				PlayReadyLog.Info("PlayReady has been activated.");
 			}
 		}
 
+
 		/// <summary>
-		/// Proactively acquires a persistent PlayReady license, provided the key ID and the license server URL.
+		/// Proactively acquires a persistent PlayReady license, provided the key ID and the license server URL. If the license
+		/// server requires a domain to be joined, this is attempted automatically after which the license request is retried.
 		/// 
 		/// Note that while this method will succeed even if a non-persistent license is returned, such a license
 		/// cannot be later used for playback. You need a persistent license for proactive license acquisition.
 		/// </summary>
-		public static async Task AcquirePersistentLicenseAsync(this HelpersContainerClasses.PlayReady container, Guid keyId, Uri licenseServerUrl, string challengeCustomData = null)
+		/// <exception cref="PlayReadyRequestException">Thrown if the license server refuses a PlayReady request.</exception>
+		public static async Task AcquirePersistentLicenseAsync(this HelpersContainerClasses.PlayReady container, Guid keyId, Uri licenseServerUrl, string acquireLicenseCustomData = null, string joinDomainCustomData = null)
 		{
 			Helpers.Argument.ValidateIsNotNull(licenseServerUrl, nameof(licenseServerUrl));
 			Helpers.Argument.ValidateIsAbsoluteUrl(licenseServerUrl, nameof(licenseServerUrl));
 
+			PlayReadyLog.Debug($"Acquiring persistent license for {keyId} from {licenseServerUrl}.");
+
 			var header = new PlayReadyContentHeader(Helpers.PlayReady.GenerateRightsManagementHeader(keyId));
-			var request = new PlayReadyLicenseAcquisitionServiceRequest();
-			request.ContentHeader = header;
-			request.Uri = licenseServerUrl;
+			var licenseRequest = new PlayReadyLicenseAcquisitionServiceRequest();
+			licenseRequest.ContentHeader = header;
+			licenseRequest.Uri = licenseServerUrl;
 
-			if (challengeCustomData != null)
-				request.ChallengeCustomData = challengeCustomData;
-
-			await request.BeginServiceRequest().IgnoreContext();
+			await Helpers.PlayReady.FulfillServiceRequestAsync(licenseRequest, acquireLicenseCustomData, joinDomainCustomData).IgnoreContext();
 		}
 
 		/// <summary>
@@ -66,6 +70,80 @@
 		{
 			var header = new PlayReadyContentHeader(Helpers.PlayReady.GenerateRightsManagementHeader(keyId));
 			return new PlayReadyLicenseIterable(header, true).Any(l => l.UsableForPlay);
+		}
+
+		private const int MSPR_E_CONTENT_ENABLING_ACTION_REQUIRED = unchecked((int)0x8004B895);
+
+		/// <summary>
+		/// Executes the standard logic requires to fulfill a PlayReady service request. You do not need to use this function
+		/// unless you are dealing with very low-level PlayReady logic - everyday app needs are cared for by other methods.
+		/// </summary>
+		/// <exception cref="PlayReadyRequestException">Thrown if the license server refuses a PlayReady request.</exception>
+		public static async Task FulfillServiceRequestAsync(this HelpersContainerClasses.PlayReady container, IPlayReadyServiceRequest serviceRequest, string acquireLicenseCustomData = null, string joinDomainCustomData = null)
+		{
+			Helpers.Argument.ValidateIsNotNull(serviceRequest, nameof(serviceRequest));
+
+			PlayReadyLog.Debug($"Fulfilling PlayReady service request of type {serviceRequest.GetType().Name}.");
+
+			while (serviceRequest != null)
+			{
+				try
+				{
+					if (serviceRequest is PlayReadyLicenseAcquisitionServiceRequest)
+					{
+						var request = (PlayReadyLicenseAcquisitionServiceRequest)serviceRequest;
+
+						if (acquireLicenseCustomData != null)
+							request.ChallengeCustomData = acquireLicenseCustomData;
+
+						if (request.Uri == null)
+						{
+							throw new EnvironmentException("No license server URL was associated with the PlayReady license request. This generally indicates that the URL is not present in the content header and must be specified manually.");
+						}
+					}
+					else if (serviceRequest is PlayReadyDomainJoinServiceRequest)
+					{
+						var request = (PlayReadyDomainJoinServiceRequest)serviceRequest;
+
+						if (joinDomainCustomData != null)
+							request.ChallengeCustomData = joinDomainCustomData;
+					}
+
+					PlayReadyLog.Debug(Helpers.Debug.ToDebugString(serviceRequest));
+
+					await serviceRequest.BeginServiceRequest().IgnoreContext();
+
+					PlayReadyLog.Debug("Successfully fulfilled PlayReady service request.");
+
+					break; // If we got to this point then we are done!
+				}
+				catch (Exception ex) when (ex.HResult == MSPR_E_CONTENT_ENABLING_ACTION_REQUIRED)
+				{
+					serviceRequest = serviceRequest.NextServiceRequest();
+
+					PlayReadyLog.Debug($"Fulfilling chained PlayReady service request of type {serviceRequest.GetType().Name}.");
+				}
+				catch (EnvironmentException)
+				{
+					throw;
+				}
+				catch (Exception ex)
+				{
+					PlayReadyLog.Error($"Failed to fulfill PlayReady service request: 0x{ex.HResult:X8}");
+
+					// If it is a request for which we have response custom data, include it in a new exception.
+					if (serviceRequest is PlayReadyLicenseAcquisitionServiceRequest)
+					{
+						throw new PlayReadyRequestException(ex, ((PlayReadyLicenseAcquisitionServiceRequest)serviceRequest).ResponseCustomData);
+					}
+					else if (serviceRequest is PlayReadyDomainJoinServiceRequest)
+					{
+						throw new PlayReadyRequestException(ex, ((PlayReadyDomainJoinServiceRequest)serviceRequest).ResponseCustomData);
+					}
+
+					throw;
+				}
+			}
 		}
 	}
 }
---
+++ Uwp/PlayReadyRequestException.cs
@@ -0,0 +1,22 @@
+﻿namespace Axinom.Toolkit
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	/// <summary>
+	/// Provides the response custom data for exceptions triggered by a PlayReady request to a remote server.
+	/// </summary>
+	public sealed class PlayReadyRequestException : Exception
+	{
+		public string ResponseCustomData { get; }
+
+		public PlayReadyRequestException(Exception original, string responseCustomData) : base(original.Message, original.InnerException)
+		{
+			Helpers.Argument.ValidateIsNotNull(original, nameof(original));
+
+			HResult = original.HResult;
+			ResponseCustomData = responseCustomData;
+		}
+	}
+}
