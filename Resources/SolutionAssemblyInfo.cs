5a36a4166304a9387702aa5f954d5f6739599133
Sander Saares <saares@axinom.com>
2016-07-05 10:51:53 +0300

    7.1.0
     * Added base64url to/from conversion helpers.
     * Added ProtectedXml helpers. You can now encrypt&sign and verify&decrypt XmlDocuments with a one-liner helper method. DotNet only, for now.
     * Added Jose helpers. You can now encrypt&sign and verify&decrypt arbitrary data packaged into JOSE objects with a one-liner helper method. DotNet only, for now. Uses jose-jwt underneath.

--- Resources/SolutionAssemblyInfo.cs
+++ Resources/SolutionAssemblyInfo.cs
@@ -1,7 +1,12 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("7.1.0")]
+// Only use major version here, with others zero, to avoid breaking references.
+[assembly: AssemblyVersion("7.0.0")]
+
+// This is the real version, displayed in UI and not used for reference purposes.
+[assembly: AssemblyFileVersion("7.1.0")]
+
 [assembly: AssemblyCompany("Axinom")]
 [assembly: AssemblyProduct("Axinom Toolkit")]
 [assembly: AssemblyCopyright("Copyright © Axinom")]
