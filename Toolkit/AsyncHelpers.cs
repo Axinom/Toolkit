0d23ee6148355099673f0f91a5a8b47a3ec72947
Sander Saares <saares@axinom.com>
2017-12-20 11:05:08 +0200

    * Helpers.Async.BackgroundThreadInvoke() now returns a Task for the created operation.

--- Toolkit/AsyncHelpers.cs
+++ Toolkit/AsyncHelpers.cs
@@ -10,11 +10,11 @@
         /// Asynchronously invokes a long-running action on a new background thread.
         /// </summary>
         /// <exception cref="ArgumentNullException">Thrown if <paramref name="action"/> is null.</exception>
-        public static void BackgroundThreadInvoke(this HelpersContainerClasses.Async container, Action action)
+        public static Task BackgroundThreadInvoke(this HelpersContainerClasses.Async container, Action action)
         {
             Helpers.Argument.ValidateIsNotNull(action, nameof(action));
 
-            Task.Factory.StartNew(action, CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+            return Task.Factory.StartNew(action, CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default);
         }
     }
 }
