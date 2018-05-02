c02a603fe04403a6bb1b34d8600605a3b7721195
Sander Saares <saares@axinom.com>
2018-05-02 12:10:00 +0300

    11.2.0  * Wildcard now supports multiple patterns as alternatives.

---
+++ Tests.NetCore/WildcardTests.cs
@@ -0,0 +1,33 @@
+﻿using Axinom.Toolkit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public sealed class WildcardTests : TestClass
+    {
+        [TestMethod]
+        public void SingleMatch_MatchesWhenExpected()
+        {
+            var wildcard = new Wildcard("abc???_tere*");
+
+            Assert.IsTrue(wildcard.IsMatch("abcabc_tere"));
+            Assert.IsTrue(wildcard.IsMatch("abcabc_tere13451451345"));
+            Assert.IsTrue(wildcard.IsMatch("abcabc_tere13451451345|||qqqq"));
+
+            Assert.IsFalse(wildcard.IsMatch("abcabcd_tere13451451345|||qqqq"));
+            Assert.IsFalse(wildcard.IsMatch("helloabcabc_tere!"));
+        }
+
+        public void MultipleMatch_MatchesWhenExpected()
+        {
+            var wildcard = new Wildcard("first?_*_xyz", "*second_?");
+
+            Assert.IsTrue(wildcard.IsMatch("first1_tere_xyz"));
+            Assert.IsTrue(wildcard.IsMatch("first1__xyz"));
+            Assert.IsTrue(wildcard.IsMatch("anotherseccond_1"));
+
+            Assert.IsFalse(wildcard.IsMatch("anotherseccond_1not"));
+        }
+    }
+}
--- Toolkit/Wildcard.cs
+++ Toolkit/Wildcard.cs
@@ -1,55 +1,81 @@
 ﻿namespace Axinom.Toolkit
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Text.RegularExpressions;
+    using System;
+    using System.Text;
+    using System.Text.RegularExpressions;
 
-	/// <summary>
-	/// A <i>*.jpeg</i> style wildcard matcher running on the <see cref="System.Text.RegularExpressions"/> engine.
-	/// Supports both the ? and * metacharacters in the wildcard expression.
-	/// </summary>
-	/// <example>
-	/// <code><![CDATA[
-	/// Wildcard wildcard = new Wildcard("*.exe");
-	/// 
-	///	foreach (string filename in Directory.GetFiles(@"C:\Windows"))
-	///	{
-	///		if (wildcard.IsMatch(filename))
-	///			Console.WriteLine("Match: {0}", filename);
-	///	}
-	/// ]]></code>
-	/// </example>
-	public sealed class Wildcard : Regex
-	{
-		/// <summary>
-		/// Initializes a wildcard with the given search pattern.
-		/// </summary>
-		/// <param name="pattern">The wildcard pattern to match. ? and * metacharacters are supported.</param>
-		public Wildcard(string pattern) : base(WildcardToRegex(pattern))
-		{
-		}
+    /// <summary>
+    /// A <i>*.jpeg</i> style wildcard matcher running on the <see cref="System.Text.RegularExpressions"/> engine.
+    /// Supports both the ? and * metacharacters in the wildcard expression.
+    /// </summary>
+    /// <example>
+    /// <code><![CDATA[
+    /// Wildcard wildcard = new Wildcard("*.exe");
+    /// 
+    ///	foreach (string filename in Directory.GetFiles(@"C:\Windows"))
+    ///	{
+    ///		if (wildcard.IsMatch(filename))
+    ///			Console.WriteLine("Match: {0}", filename);
+    ///	}
+    /// ]]></code>
+    /// </example>
+    public sealed class Wildcard : Regex
+    {
+        /// <param name="pattern">The wildcard pattern to match. ? and * metacharacters are supported.</param>
+        public Wildcard(string pattern) : base(WildcardToRegex(pattern))
+        {
+        }
 
-		/// <summary>
-		/// Initializes a wildcard with the given search pattern and options.
-		/// </summary>
-		/// <param name="pattern">The wildcard pattern to match. ? and * metacharacters are supported.</param>
-		/// <param name="options">A combination of one or more <see cref="RegexOptions"/>.</param>
-		public Wildcard(string pattern, RegexOptions options)
-			: base(WildcardToRegex(pattern), options)
-		{
-		}
+        /// <param name="patterns">One or more wildcard patterns to match. ? and * metacharacters are supported.</param>
+        public Wildcard(params string[] patterns) : base(WildcardToRegex(patterns))
+        {
+        }
 
-		/// <summary>
-		/// Converts a wildcard to a regex.
-		/// </summary>
-		/// <param name="pattern">The wildcard pattern to convert.</param>
-		/// <returns>A regex equivalent of the given wildcard.</returns>
-		private static string WildcardToRegex(string pattern)
-		{
-			return "^" + Escape(pattern).
-				Replace("\\*", ".*").
-				Replace("\\?", ".") + "$";
-		}
-	}
+        /// <param name="pattern">The wildcard pattern to match. ? and * metacharacters are supported.</param>
+        /// <param name="options">A combination of one or more <see cref="RegexOptions"/>.</param>
+        public Wildcard(string pattern, RegexOptions options)
+            : base(WildcardToRegex(pattern), options)
+        {
+        }
+
+        /// <param name="patterns">One or more wildcard patterns to match. ? and * metacharacters are supported.</param>
+        /// <param name="options">A combination of one or more <see cref="RegexOptions"/>.</param>
+        public Wildcard(string[] patterns, RegexOptions options)
+            : base(WildcardToRegex(patterns), options)
+        {
+        }
+
+        private static string WildcardToRegexVariant(string pattern)
+        {
+            return Escape(pattern).
+                Replace("\\*", ".*").
+                Replace("\\?", ".");
+        }
+
+        private static string WildcardToRegex(string pattern)
+        {
+            return "^" + WildcardToRegexVariant(pattern) + "$";
+        }
+
+        private static string WildcardToRegex(string[] patterns)
+        {
+            if (patterns.Length == 0)
+                throw new ArgumentException("At least one pattern must be specified to define a wildcard match.", nameof(patterns));
+
+            var sb = new StringBuilder();
+            sb.Append("^(?:");
+
+            for (var i = 0; i < patterns.Length; i++)
+            {
+                sb.Append(WildcardToRegexVariant(patterns[i]));
+
+                if (i != patterns.Length - 1)
+                    sb.Append('|');
+            }
+
+            sb.Append(")$");
+
+            return sb.ToString();
+        }
+    }
 }
