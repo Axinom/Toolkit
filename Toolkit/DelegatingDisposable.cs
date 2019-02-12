1e5fb0bddcde3843351563e36f83d134c1e5e808
Sander Saares <saares@axinom.com>
2019-02-12 08:28:15 +0200

    DelegatingDisposable

---
+++ Toolkit/DelegatingDisposable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Axinom.Toolkit
+{
+    public sealed class DelegatingDisposable : IDisposable
+    {
+        public DelegatingDisposable(Action onDispose)
+        {
+            _onDispose = onDispose;
+        }
+
+        private readonly Action _onDispose;
+
+        public void Dispose()
+        {
+            _onDispose?.Invoke();
+        }
+    }
+}
