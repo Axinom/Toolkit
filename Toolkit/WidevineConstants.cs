2aac0721db35e12f9205c4168d4fae37f7cc0f62
Sander Saares <saares@axinom.com>
2019-06-15 14:37:01 +0300

    14.1.0 Add WidevineConstants.SystemId

---
+++ Toolkit/WidevineConstants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Axinom.Toolkit
+{
+    public static class WidevineConstants
+    {
+        public static readonly Guid SystemId = new Guid("edef8ba9-79d6-4ace-a3c8-27dcd51d21ed");
+    }
+}
