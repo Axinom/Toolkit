f2965bd148f1b088b99b2fe22359e38b022559c1
Sander Saares <sander@saares.eu>
2020-02-03 09:58:33 +0200

    14.1.1

--- Resources/SolutionAssemblyInfo.cs
+++ Resources/SolutionAssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 
 // This is the real version, displayed in UI and not used for reference purposes.
-[assembly: AssemblyFileVersion("14.1.0")]
+[assembly: AssemblyFileVersion("14.1.1")]
 
 // Only use major version here, with others zero, to avoid breaking references.
 [assembly: AssemblyVersion("14.0.0")]
