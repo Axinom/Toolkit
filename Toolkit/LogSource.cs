5ad3600c566889eb97aa3bab9372d67c42241883
Sander Saares <sander@saares.eu>
2020-02-13 12:06:12 +0200

    * Added LogSource(LogSource wrapped) constructor for easy LogSource wrapping.

--- Toolkit/LogSource.cs
+++ Toolkit/LogSource.cs
@@ -85,6 +85,13 @@ namespace Axinom.Toolkit
             _name = name;
         }
 
+        protected LogSource(LogSource wrapped)
+        {
+            Helpers.Argument.ValidateIsNotNull(wrapped, nameof(wrapped));
+
+            _name = wrapped._name;
+        }
+
         private readonly string _name;
 
         internal abstract void Write(LogEntrySeverity severity, string source, string message);
